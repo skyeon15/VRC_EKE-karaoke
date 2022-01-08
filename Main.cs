a37c518b120ae2da2c2e7b6918a49ae044dbf3c8
에케EKE <eke@bbforest.net>
2022-01-09 02:06:48 +0900

    fix: Change Video Player

--- Main.cs
+++ Main.cs
@@ -37,38 +37,43 @@ namespace EKE_karaoke
                 name = data.Select("td").Eq(2).Text;
                 if (num != "")
                 {
-                    vrcurl+= $"\t\tVRCUrl n{num} = new VRCUrl(\"{url}\");\n" +
-                        $"\t\tVRCUrl q{num} = new VRCUrl(\"https://t-ne.x0.to/?url={url}\");\n";
-                    request1 += $"\t\t\t\t\tcase \"{num}\":" +
-                        $"\n\t\t\t\t\t\ttargetVideoPlayer.PlayVideo(q{num});" +
-                        $"\n\t\t\t\t\t\tbreak;\n";
+                    vrcurl+= $"VRCUrl n{num} = new VRCUrl(\"{url}\");\n" +
+                        $"VRCUrl q{num} = new VRCUrl(\"https://t-ne.x0.to/?url={url}\");\n";
+                    request1 += $"case \"{num}\":\n" +
+                    $"if (_quest) addURL(q{num});\n" +
+                    $"else addURL(n{num});\n" +
+                    $"Debug.Log($\"에케 디버그: Request: {num}\");\n" +
+                    $"break;\n";
+                    /*
                     request2 += $"\t\t\t\t\tcase \"{num}\":" +
-                        $"\n\t\t\t\t\t\ttargetVideoPlayer.PlayVideo(n{num});" +
-                        $"\n\t\t\t\t\t\tbreak;\n";
+                        $"\n\t\t\t\t\t\twolfePlayerController.AppendToQueue(n{num});" +
+                        $"\n\t\t\t\t\t\tbreak;\n";*/
                 }
             }
-
-            doc = NSoupClient.Parse(new Uri("https://docs.google.com/spreadsheets/d/e/2PACX-1vQlA-OAzCK7SEBwW_BdCGUH8R60VaAjfgkSGzzlwRypRHhVsftawVn3_yicQg4YDgg4pHFC4RguOa2S/pubhtml?gid=515940993&single=true"), 5000);
-            datas = doc.Select("tbody tr");
-            i = 0;
-            foreach (Element data in datas)
-            {
-                i++;
-                if (i < 3) continue;
-                num = data.Select("td").Eq(2).Text;
-                name = data.Select("td").Eq(0).Text;
-                if (num != "")
+            /*
+                doc = NSoupClient.Parse(new Uri("https://docs.google.com/spreadsheets/d/e/2PACX-1vQlA-OAzCK7SEBwW_BdCGUH8R60VaAjfgkSGzzlwRypRHhVsftawVn3_yicQg4YDgg4pHFC4RguOa2S/pubhtml?gid=515940993&single=true"), 5000);
+                datas = doc.Select("tbody tr");
+                i = 0;
+                foreach (Element data in datas)
                 {
-                    songname += $"\t\t\t\tcase {num}:" +
-                        $"\n\t\t\t\t\treturn \"{name}\";\n";
+                    i++;
+                    if (i < 3) continue;
+                    num = data.Select("td").Eq(2).Text;
+                    name = data.Select("td").Eq(0).Text;
+                    if (num != "")
+                    {
+                        songname += $"\t\t\t\tcase {num}:" +
+                            $"\n\t\t\t\t\treturn \"{name}\";\n";
+                    }
                 }
-            }
 
+                {TextBox_Songname.Text}\n
+            */
             TextBox_VRCUrl.Text = TextBox_VRCUrl.Text.Replace("문자열삽입위치", vrcurl);
             TextBox_Songname.Text = TextBox_Songname.Text.Replace("문자열삽입위치", songname);
-            TextBox_Request.Text = TextBox_Request.Text.Replace("문자열삽입위치1", request1);
-            TextBox_Request.Text = TextBox_Request.Text.Replace("문자열삽입위치2", request2);
-            Result.Text = $"#region 노래등록\n{TextBox_VRCUrl.Text}\n{TextBox_Songname.Text}\n{TextBox_Request.Text}\n#endregion";
+            TextBox_Request.Text = TextBox_Request.Text.Replace("문자열삽입위치", request1);
+            //TextBox_Request.Text = TextBox_Request.Text.Replace("문자열삽입위치2", request2);
+            Result.Text = $"#region 노래등록\n{TextBox_VRCUrl.Text}\n{TextBox_Request.Text}\n#endregion";
         }
     }
 }
